8de8691ae04798e6814b7440ec6eccc3b139bb35
Yeongho Kim <yeonghoey@gmail.com>
2019-08-17 20:23:38 +0900

    Implement snapshot on 'resize_export_back.lua'

--- scripts/resize_export_back.lua
+++ scripts/resize_export_back.lua
@@ -1,6 +1,18 @@
 app.command.FitScreen()
+
 app.command.SpriteSize()
 app.command.FitScreen()
+
 app.command.SaveFileCopyAs()
+
+-- Take a snapshot
+local sprite = app.activeSprite
+local filename = sprite.filename
+local _, _, dirname, basename = string.find(filename, "(.*)/(.*)")
+local _, _, name, ext = string.find(basename, "(.*)%.(.*)")
+local now = os.date("%Y%m%d-%H%M%S")
+local snapshotname = string.format("%s/%s/%s.png", dirname, name, now)
+sprite:saveCopyAs(snapshotname)
+
 app.undo()
 app.command.FitScreen()
