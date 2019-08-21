25f7c3c12b6687d6be77a09e0cc40d3f4356b7ac
yeonghoey <yeonghoey@gmail.com>
2019-08-22 03:46:09 +0900

    Add -001 postfix

--- scripts/resize_export_back.lua
+++ scripts/resize_export_back.lua
@@ -11,7 +11,9 @@ local filename = sprite.filename
 local _, _, dirname, basename = string.find(filename, "(.*)/(.*)")
 local _, _, name, ext = string.find(basename, "(.*)%.(.*)")
 local now = os.date("%Y%m%d-%H%M%S")
-local snapshotname = string.format("%s/%s/%s.png", dirname, name, now)
+-- Add "-001" to the snapshotname because sequential numbered files will be
+-- generated if the sprite has multiple frames.
+local snapshotname = string.format("%s/%s/%s-001.png", dirname, name, now)
 sprite:saveCopyAs(snapshotname)
 
 app.undo()
