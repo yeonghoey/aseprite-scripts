5b5281eec4a57de0d87265ee5f678144384236ed
Yeongho Kim <yeonghoey@gmail.com>
2019-08-25 08:18:23 +0900

    Add size to snapshotname

--- scripts/snap.lua
+++ scripts/snap.lua
@@ -1,6 +1,6 @@
 -- snap.lua
 -- Resize and export all the frames as png files which are named
--- filename/yyyymmdd-HHMMSS-NNN.png, and then rollback the size.
+-- filename/yyyymmdd-HHMMSS-WWWxHHH-NNN.png, and then rollback the size.
 
 app.command.SpriteSize()
 
@@ -9,10 +9,13 @@ local filename = sprite.filename
 local _, _, dirname, basename = string.find(filename, "(.*)/(.*)")
 local _, _, name, ext = string.find(basename, "(.*)%.(.*)")
 local now = os.date("%Y%m%d-%H%M%S")
+local width = sprite.width
+local height = sprite.height
 -- Add "-001" to the snapshotname because sequential numbered files will be
 -- generated if the sprite has multiple frames.
 -- The index starts from 1 to match with frame number.
-local snapshotname = string.format("%s/%s/%s-001.png", dirname, name, now)
+local snapshotname = string.format("%s/%s/%s-%dx%d-001.png",
+                                   dirname, name, now, width, height)
 sprite:saveCopyAs(snapshotname)
 
 app.undo()
