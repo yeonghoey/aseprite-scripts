0cc2e417ecb6e24ead0e940b5177fcfd76b39ca2
Yeongho Kim <yeonghoey@gmail.com>
2019-08-25 08:07:19 +0900

    Refactor resize_export_back.lua and rename it to snap.lua

--- scripts/snap.lua
+++ scripts/snap.lua
@@ -1,11 +1,9 @@
-app.command.FitScreen()
+-- snap.lua
+-- Resize and export all the frames as png files which are named
+-- filename/yyyymmdd-HHMMSS-NNN.png, and then rollback the size.
 
 app.command.SpriteSize()
-app.command.FitScreen()
 
-app.command.SaveFileCopyAs()
-
--- Take a snapshot
 local sprite = app.activeSprite
 local filename = sprite.filename
 local _, _, dirname, basename = string.find(filename, "(.*)/(.*)")
@@ -13,8 +11,8 @@ local _, _, name, ext = string.find(basename, "(.*)%.(.*)")
 local now = os.date("%Y%m%d-%H%M%S")
 -- Add "-001" to the snapshotname because sequential numbered files will be
 -- generated if the sprite has multiple frames.
+-- The index starts from 1 to match with frame number.
 local snapshotname = string.format("%s/%s/%s-001.png", dirname, name, now)
 sprite:saveCopyAs(snapshotname)
 
 app.undo()
-app.command.FitScreen()
